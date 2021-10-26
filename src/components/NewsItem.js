a5cdf825bf10ab61d2f32b7c96ea6c8b1a97833f
vivek-dew <vkmaurya@dewsolutions.in>
2021-10-26 07:14:19 +0530

    add title, author, news source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imageUrl, newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
-                    <img src={imageUrl? imageUrl : `https://images.livemint.com/img/2021/10/25/600x338/MINI_1635156873204_1635156895423.jpg`} onError={(e)=>{e.target.onerror = null; e.target.src="https://images.livemint.com/img/2021/10/25/600x338/MINI_1635156873204_1635156895423.jpg"}} className="card-img-top" alt="..."/>
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: 1}}>
+                        {source}
+                    <span class="visually-hidden">unread messages</span>
+                    </span>
+                    <img src={imageUrl ? imageUrl : `https://images.livemint.com/img/2021/10/25/600x338/MINI_1635156873204_1635156895423.jpg`} onError={(e) => { e.target.onerror = null; e.target.src = "https://images.livemint.com/img/2021/10/25/600x338/MINI_1635156873204_1635156895423.jpg" }} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : 'Unknown'} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Go somewhere</a>
-                    </div> 
+                    </div>
                 </div>
             </div>
         )
